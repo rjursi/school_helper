eabd9c7bfd8fb75df545c87b908d4571b1e4e18b
YunKwon Kim <dfg3790@gmail.com>
2019-07-15 14:48:51 +0900

    socket_rebconnection

--- client.py
+++ client.py
@@ -1,36 +1,56 @@
 import socket
 import os
 import subprocess
+import sys
+from select import *
+from multiprocessing import Process, Queue
+import multiprocessing
+from time import ctime
+
+
+
 
 class Client:
-    def GetServerInfo(self):
-        ServerInfo = open("./ServerInfo.txt",'r')
-        for line in ServerInfo:
-            ServerInfoResult = line.split(" ")
-        ServerInfoResult = ServerInfoResult[:2]
-        ServerInfoResult[1] = int(ServerInfoResult[1])
-        
-        return tuple(ServerInfoResult)
-        
-        
+    BUFSIZE = 1024
 
-    def runClient(self):
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def client_recv(self,clientSocket,server_ip_port_info):
         
-        clientSocket.connect(self.GetServerInfo())
-
-        print('연결 확인 됐습니다.')
-
-
         while True:
-            data = clientSocket.recv(2048)
-            # 서버 측의 cmd 명령을 받음
+            try:
+                data = clientSocket.recv(self.BUFSIZE)
+            except ConnectionResetError:
+                print("[INFO][%s] 서버의 연결이 종료되었습니다." % ctime())
+                clientSocket.close()
+                sys.exit()
+
+
+            if not data:
+                print('서버 (%s:%s)와의 연결이 끊어졌습니다.' % server_ip_port_info)
+                clientSocket.close()
+                sys.exit()
 
             if data[:2].decode("cp949") == 'cd':
             #만약 서버에서 보낸 요청에서 첫 문자가 cd 일 경우 
                 os.chdir(data[3:].decode("cp949"))
+                continue
+
             if len(data) > 0:
+                
+                if data.decode("cp949") == "quit":
+                    # 서버 측에서 종료가 될 경우에는 bytes 형식으로 ''을 보냄, 즉 다음과 같이 검출
+                    print('서버 (%s:%s)와의 연결이 끊어졌습니다.' % server_ip_port_info)
+                    clientSocket.close()
+                    sys.exit()
+                    # 서버 측으로부터 quit 메세지를 받았을때 
+                    break
+                    return
+               
+            
+                # 클라이언트 cmd 창도 종료
+
                 # cd 명령이 아닌 다른 명령일 경우
+                # 서버로부터 요청 명령이 날라올 경우 subprocess를 통해서 프로세스 하나를 실행
+                
 
                 cmd = subprocess.Popen(data[:].decode("cp949"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
@@ -44,10 +64,46 @@ class Client:
                 # 클라이언트가 수행한 작업을 서버측에서 확인이 가능하도록 다시 보내줌
                 # os.getcwd() : 자신의 현재 디렉토리 위치를 보여줌, 즉 프롬프트처럼 표시되도록 하는것
 
-
                 print(server_request_str)
                 # 서버측에서 요청한 명령을 실행
+        
+    def client_quit(self,clientSocket):
+        while True:
+            cmd = input()
+
+            if cmd == "quit":
+                clientSocket.close()
+                return
+    
+
+    def __init__(self):
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        server_ip_port = input("서버 IP와 포트 번호 입력 : ").split()
+        server_ip_port[1] = int(server_ip_port[1])
+        
+        server_ip_port_info = tuple(server_ip_port)
+        
+        try:
+
+            clientSocket.connect(server_ip_port_info)
+        except Exception as e:
+            print('서버(%s:%s) 에 연결할 수 없습니다.' % server_ip_port_info)
+            sys.exit()
+            
+        print('서버(%s:%s) 에 연결 되었습니다.' % server_ip_port_info)
+        
+        # while connection_list:
+        try:
+            # read_socket, write_socket, error_socket = select(connection_list, [], [], 10)
+            
+            self.client_recv(clientSocket, server_ip_port_info)
+
+            print("recv success")
+        except KeyboardInterrupt:
+            print("Ctrl + C 로 인한 클라이언트 종료")
+            clientSocket.close()
+            sys.exit()
 
 if __name__ == "__main__":
     CtlClient = Client()
-    CtlClient.runClient()
--- server.py
+++ server.py
@@ -1,101 +1,126 @@
-import socket
+from socket import *
+from select import *
+from time import ctime
 import sys
 
-class Server:
-    server_ip = ''
-    server_info = []
-    
-    def ServerInfoSave(self):
-        ServerInfo = open("./ServerInfo.txt",'w+')
 
-        for data in self.server_info:
-            ServerInfo.write(str(data) + ' ')
-        ServerInfo.close()
-            
+class Server:
+    SERVER_IP = ''
+    SERVER_PORT = 12345
+    BUFSIZE = 1024
+
 
     def getHostIP(self):
         # 호스트의 IP 주소만을 얻기위한 함수
         
-        TestSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        TestSocket = socket(AF_INET, SOCK_STREAM)
         TestSocket.connect(("gmail.com",80))
         self.server_ip = str(TestSocket.getsockname()[0]) # ip 값 만을 가져옴
         print("서버 IP : %s" % self.server_ip)
         
         TestSocket.close()
-
-    def send_command(self, serverSocket, connectionSocket):
-        while True:
-            cmd = input()
-            if cmd == 'quit':
-                connectionSocket.close()
-                serverSocket.close()
-                sys.exit()
-
-                # quit 명령이 들어오면 세션 종료
+  
+    def client_connect(self, read_socket, serverSocket):
+        print(read_socket)
+        for sock in read_socket:
+            if sock == serverSocket:
+                clientSocket, addr_info = serverSocket.accept()
+                read_socket.append(clientSocket)
                 
-            if len(str.encode(cmd)) > 0:
-                connectionSocket.send(str.encode(cmd))
-                client_response = connectionSocket.recv(2048)
-                print(client_response)
-                client_response = str(client_response.decode("cp949"))
-                # 클라이언트로부터 받은 메세지를 변수에 저장
+                print('[INFO][%s] 클라이언트(%s) 가 새롭게 연결되었습니다.' % (ctime(), addr_info[0]))
+                continue
+            
+                # 리스트에서 소켓 리스트를 읽음, 그러나 serverSocket밖에 없다면 clientSocket를 새로 추가함
+            
+            else:
+                while True:
+                    msg = input()
+                    
+                    if msg == "quit":
+                        print("[INFO][%s] 서버의 연결이 종료되었습니다." % ctime())
+                        sock.close()
+                        read_socket.remove(sock)
+                        serverSocket.close()
+                        sys.exit()
+                        
 
-                print(client_response, end="")
-                
-                # 클라이언트에서 실행한 명령의 결과를 서버측에서도 확인하기 위하여 출력
-
-
-    def runServer(self):
-
-        try:
-            serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error as msg:
-            print("socket creation error: " + str(msg))
-
+                    if msg:
+                        
+                        for socket_in_list in read_socket:
+                            
+                            if socket_in_list != serverSocket:
+                                try:
+                                    socket_in_list.send(msg.encode("cp949"))
+                                    
+                                except Exception as e:
+                                    print(e.message)
+                                    socket_in_list.close()
+                                    read_socket.remove(socket_in_list)
+                                    continue
+                        continue
+                    
+                    else:
+                        read_socket.remove(sock)
+                        sock.close()
+                        print('[INFO][%s] 사용자와의 연결이 끊어졌습니다.' % ctime())
+                    
+                    
+        
+    def __init__(self):
+        
         self.getHostIP()
 
         #현재 서버의 IP 수집
-        #서버 소켓 객체 생성, AF_INET = IPV4, SOCK_STREAM = TCP 통신
-        try:
-            serverSocket.bind((self.server_ip,0))   
-        except socket.error as msg:
-            print("Socket binding error : " + str(msg) + "\n" + "Retrying.....")
-            serverSocket.socket_bind()
-
-        for data in serverSocket.getsockname():
-            self.server_info.append(data)
         
-        # ip 주소와 port 번호를 server_info 클래스 리스트 변수에 저장
 
-        self.server_info = tuple(self.server_info)
+        server_ip_port = (self.SERVER_IP, self.SERVER_PORT)
         
-        self.ServerInfoSave()
+        serverSocket = socket(AF_INET, SOCK_STREAM)
+        # 서버 소켓 객체 생성, AF_INET = IPV4, SOCK_STREAM = TCP 통신
+       
 
-
-        # 클라이언트에게 서버 IP와 포트값 넘기기 위해 모듈 사용
-
-        #bind 는 서버 소켓과 AF를 연결하는 과정,'' = INADDR_ANY, 어떤 IP 든 받는다.
-        #0의미는 해당 호스트에서 확인을 하여 랜덤으로 포트를 할당을 한다.
-        #목록을 튜플로 할당
-
+        serverSocket.bind(server_ip_port)
+        # 서버 IP 와 Port 할당
+        
         serverSocket.listen(50)
-        #총 50개의 동시접속자까지 허용을 한다.
-
         
+        # 50개의 호스트의 요청을 기다림
 
-        connectionSocket, addr = serverSocket.accept()
-        # accept() : 소켓에 누군가가 접속하여 연결되었을때 결과값이 return 되는 함수, 클라이언트와 서버의 연결이 성립이 될 경우 
-        #            값을 반환
+        connection_list = [serverSocket]
+    
+        # 클라이언트들이 연결되는 리스트
 
-        # connectionSocket 에는 클라이언트로부터 데이터를 받을 객체를 생성, 초기값으로 서버에 대한 주소와 포트(laddr), 클라이언트의 주소 및 포트 정보(raddr)가 들어가있음
-
-
-        print(str(addr[0]), '에서 접속이 확인되었습니다.')
-        #print(str(connectionSocket))
-
-        self.send_command(serverSocket,connectionSocket)
+        print("====================================")
+        print("서버 IP  : %s\n" % self.SERVER_IP)
+        print("Port : %s\n" % self.SERVER_PORT)
+        print("접속을 기다립니다.")
+        print("====================================")
         
+        while connection_list:
+        # 클라이언트의 응답을 계속 받을 수 있도록 무한루프로 동작
+        
+            try:
+                
+                read_socket, write_socket, error_socket = select(connection_list, [], [], 10)
+                # select 로 요청을 받고, 10초마다 블럭킹을 해제하도록 함
+                # I/O 다중화 구현할 때 select 이용
+
+                # 즉 10초마다 다른 소켓에 클라이언트가 접속이 되었는지 체크하는 것을 말함
+
+                # select 함수에서 자체적으로 3개의 값을 return 을 하며, 각각 3개의 소켓을 반환함
+                # connection_list 를 매개변수로 넣었는데, 여기 리스트에 있는 소켓중 하나에 접속이 발생할때까지 대기
+                # 접속이 되기 전까지는 block 상태
+                # 클라이언트가 접속할 때에만 각 클라이언트에 적합한 작업을 수행
+
+                self.client_connect(read_socket, serverSocket)
+            except KeyboardInterrupt:
+                print("Ctrl + C 입력으로 인한 종료")
+                serverSocket.close()
+
+                sys.exit()
+                 
 if __name__ == "__main__":
+
     CtlServer = Server() # 서버 객체 생성
-    CtlServer.runServer()
+   
     
